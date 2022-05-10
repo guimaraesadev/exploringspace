8cedebf2135331cf7ab2505f5d6e35aaed64d4fa
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-05-09 23:44:21 -0300

    add: html, css & JS files

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+// minha chave: ILh9sgq501KWB7ekanKcSduk0pqkhe4e7V26GL4F
+// minha chave in web: https://api.nasa.gov/planetary/apod?api_key=ILh9sgq501KWB7ekanKcSduk0pqkhe4e7V26GL4F
+
+//function para consultar API
+// requisição AJAX
+// página main com a data atual de cada dia
+
+function consultarAPI() {
+    let dateInput = $("#inputdate").val()
+    const chaveKey = ILh9sgq501KWB7ekanKcSduk0pqkhe4e7V26GL4F
+
+
+    //requisição AJAX: ../JqueryCurso/AJAX => exemplo
+
+    $.ajax({
+
+    });
+}
