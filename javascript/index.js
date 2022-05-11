d883e7d5da2615725cb5547a3eff1ecd88bcb477
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-05-11 12:12:03 -0300

    add js file: correção de 'copyright'

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,44 @@
-// minha chave: ILh9sgq501KWB7ekanKcSduk0pqkhe4e7V26GL4F
-// minha chave in web: https://api.nasa.gov/planetary/apod?api_key=ILh9sgq501KWB7ekanKcSduk0pqkhe4e7V26GL4F
-
-//function para consultar API
-// requisição AJAX
-// página main com a data atual de cada dia
-
-function consultarAPI() {
-    let dateInput = $("#inputdate").val()
-    const chaveKey = ILh9sgq501KWB7ekanKcSduk0pqkhe4e7V26GL4F
-
-
-    //requisição AJAX: ../JqueryCurso/AJAX => exemplo
-
+$("#btnSend").click(function ()
+{
+    useAPI();
+  });
+    //retorna dados da api
+    function useAPI() {
+    const data = $("#date").val();
+  
+    
     $.ajax({
+      url: `https://api.nasa.gov/planetary/apod?api_key=yIvc5SJRvBfiStShBj7FvM1UWme8alglgrpaV9LH&date=${data}`,
+        success: function (data) {
+          
+        $("#texto").html(" ")
+        apiData(data);
+  
+        }, error: function () {
+            $("#texto").html("Error. Enter a valid date!")
+      }
+      
+    })
+  
+    
+    function apiData(procura) {
+        const imagem = $("#mediaAPI");
+        const titulo = $("#title");
+        const explicacao = $("#contentP");
+        const autor = $("#autorP")
+        
+  
+        titulo.html(procura.title);
+        explicacao.html(`<strong>Explanation:</strong> ${procura.explanation}`);
+        autor.html(`<strong>Copyright:</strong> ${procura.copyright}`);
 
-    });
-}
+        
+  
+      if (procura.media_type == 'image') {
+        imagem.html(`<img class="img" src="${procura.url}"/>`)
+      } else {
+        imagem.html(`<iframe class="img" src="${procura.url}?autoplay=1&mute=1"></iframe>`)
+      }
+    }
+    
+  }
